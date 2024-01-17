d9034994323385bf59366182e6ed2d5f834b1826
Juniper Bender <jayjayb100@msn.com>
2024-01-16 18:08:37 -0600

    Maybe?

--- tests/time-utils/extract-time-from-input.test.ts
+++ tests/time-utils/extract-time-from-input.test.ts
@@ -8,8 +8,7 @@ describe('extractTimeFromInput Test Suite', () => {
         expect(true).toEqual(true)
     })
     beforeAll(() => {
-        advanceTo(new Date(Date.UTC(2024, 1, 16, 12, 0, 0)));
-        // advanceTo(new Date("2024-01-16T12:00:00"));    // Mock the date
+        advanceTo(new Date("2024-01-16T12:00:00"));    // Mock the date
     });
 
     afterAll(() => {
